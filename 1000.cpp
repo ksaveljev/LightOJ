7f15cb35db8aa0f2f2d9ce55c4fe2551bb6c0fff
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2014-12-29 14:09:27 +0200

    1000 solved

---
+++ 1000.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+#define pow2(i) (1<<i)
+#define bit(i) (1<<i)
+#define isOdd(i) (i&1)
+#define isEven(i) (!(i&1))
+#define sz(i) i.size()
+#define REP(i, b, n) for (int i = b; i < n; i++)
+#define REPI(i, b, n) for (int i = b; i <= n; i++)
+#define rep(i, n) REP(i, 0, n)
+#define repi(i, n) REPI(i, 0, n)
+
+int main(void) {
+    int number_of_cases, a, b;
+
+    cin >> number_of_cases;
+
+    REPI (i, 1, number_of_cases) {
+        cin >> a >> b;
+        cout << "Case " << i << ": " << a + b << endl;
+    }
+    return 0;
+}
